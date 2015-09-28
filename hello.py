1ad3c99da8f7fb3581f8fd2c35438fbcd1a830c3
khumphries <khumphries@gannett.com>
2015-09-28 14:18:11 -0400

    Frist develop commit

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 __author__ = '__knh4vu__'
 
 from helper import greeting
+from helper2 import greeting2
 
 if __name__ == '__main__':
 	greeting('hello')
+	greeting2('There')
---
+++ helper2.py
@@ -0,0 +1,2 @@
+def greeting2(msg):
+	print(msg)
